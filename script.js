dc401009c4f410da40067749e86c7be54b69bef9
Satyakoidala <koidalasai21@gmail.com>
2022-10-09 14:05:26 +0530

    fixed day to day refresh, integrated codeforces api to fetch problems

--- script.js
+++ script.js
@@ -3,76 +3,75 @@ const problemStatement = document.querySelector(".problem-statement");
 
 
 
-console.log("Getting new Problem...");
+console.log("Trying to get a new Problem...");
+
+// global variable to store the problem URL.
+var problemURL;
 
 
-const urlList = [
-      `https://codeforces.com/problemset/problem/1506/D`,
-      `https://codeforces.com/problemset/problem/1520/D`,
-      //1200
+/**
+ * If the current date is different than the date stored in localStorage, then update localStorage with
+ * the current date and return true. Otherwise, return false
+ * @returns a boolean value.
+ */
+function hasOneDayPassed() {
+	var date = new Date().toLocaleDateString();
 
-      'https://codeforces.com/problemset/problem/1497/C1',
-      'https://codeforces.com/problemset/problem/1536/B',
-      'https://codeforces.com/problemset/problem/1514/B',
-      'https://codeforces.com/problemset/problem/1472/D',
-      'https://codeforces.com/problemset/problem/1559/C',
-      'https://codeforces.com/problemset/problem/1553/C',
-      'https://codeforces.com/problemset/problem/1613/C',
-      'https://codeforces.com/problemset/problem/1619/C',
+	if (localStorage.previousDate == date)
+		return false;
 
-      
-
-
-];
-
-
-
-const problemList = [
-      "D. Epic Transformation",
-      "D. Same Differences",
-      //1200
-      "C1. k-LCM (easy version)",
-      "B. Prinzessin der Verurteilung",
-      "B. AND 0, Sum Big",
-      "D. Even-Odd Game",
-      "C. Mocha and Hiking",
-      "C. Penalty",
-      "C. Poisoned Dagger",
-      "C. Wrong Addition",
-
-
-
-];
-
-var previousDate = new Date().getDate()
-var index = Math.floor(Math.random() * problemList.length);
-//index = (index + 1) % problemList.length;
-var CodeForcesUrl = urlList[index]
-problemStatement.innerText = problemList[index];
-
-
-function update() {
-      var currentDate = new Date().getDate();
-      console.log("Entering into CodeForces...");
-      console.log(currentDate, previousDate);
-
-      //console.log( currentDate + "  " + previousDate + " " + index)
-      if (currentDate != previousDate) {
-            console.log("FDEe")
-            index = (index + 1) % problemList.length;
-            problemStatement.innerText = problemList[index];
-            CodeForcesUrl = urlList[index]
-            //previousIndex = index;
-            previousDate = currentDate;
-      }
+	localStorage.previousDate = date;
+	return true;
 }
-setInterval(update, 2000);
+
+
+async function updateProblem() {
+	// checking if old problem data is existing, if exists display it.
+	if (localStorage.selectedProblem) {
+		// parsing object data from localstorage
+		const selectedProblem = JSON.parse(localStorage.selectedProblem);
+
+		// re-taking the same steps to represent problem object
+		const htmlText = `${selectedProblem.contestId}. ${selectedProblem.index}. ${selectedProblem.name}`;
+		problemStatement.innerText = htmlText;
+		problemURL = `https://codeforces.com/problemset/problem/${selectedProblem.contestId}/${selectedProblem.index}`;
+	}
+
+	// checking one day condition, if this check passes the new problem is fetched.
+	if (!hasOneDayPassed()) return false;
+
+	// using codeforces api to fetch problems
+	const response = await fetch('https://codeforces.com/api/problemset.problems');
+	const data = await response.json();
+
+	// obtaining problem for the day from the list of problems
+	const problemList = data.result.problems;
+	const index = Math.floor(Math.random() * problemList.length);
+	const selectedProblem = problemList[index];
+
+	// using localStorage to store the selected problem
+	localStorage.selectedProblem = JSON.stringify(selectedProblem);
+
+	// codeforces link is created and stored
+	problemURL = `https://codeforces.com/problemset/problem/${selectedProblem.contestId}/${selectedProblem.index}`;
+
+	// creating htmlText and updating the problem statement on the page
+	const htmlText = `${selectedProblem.contestId}. ${selectedProblem.index}. ${selectedProblem.name}`;
+	problemStatement.innerText = htmlText;
+
+	// console the problem details on every new problem fetch
+	console.log(selectedProblem);
+}
+
+// on every page load, it tries to update the problem
+window.addEventListener('DOMContentLoaded', updateProblem);
+
+// keep checking every minute, and trying to load a new problem
+setInterval(updateProblem, 60000);
 
 function enterIntoTheProblem() {
-      window.open(CodeForcesUrl, "_blank");
-      
+	window.open(problemURL, "_blank");
 }
-
 
 solveButton.addEventListener("click", enterIntoTheProblem);
 
